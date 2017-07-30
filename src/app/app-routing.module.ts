34cd811b2e79a15c80900063b512030f54be0c0a
Dima <aws.inspiration@gmail.com>
2017-07-30 15:46:13 +0300

    ready two apps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageComponent } from './page/page.component';
+import { PageIdComponent } from './page-id/page-id.component';
+import { SeoPageIdGuard } from './services/seo-page-id-guard';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', component: HomeComponent,
+    data: {
+      title: ['Home page'],
+      desc: 'My First Static Website built with Angular Universal'
+    }
+  },
+  
+  {
+    path: 'page.html', component: PageComponent,
+    data: {
+      title: ['Elements List'],
+      desc: 'My List of elements in my Static Website'
+    }
+  },
+
+  {
+    path: 'page/:id', component: PageIdComponent, canActivate: [SeoPageIdGuard]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+import { ServerModule } from '@angular/platform-server';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { PageIdComponent } from './page-id/page-id.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HomeComponent,
+    PageIdComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'angular-cli-universal' }),
+    ServerModule,
     AppRoutingModule
   ],
   providers: [],
